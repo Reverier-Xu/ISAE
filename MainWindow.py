21608132a2eab39475b8311c42a78c07dc5d7030
Reverier-Xu <reverier.xu@outlook.com>
2020-03-27 23:19:51 +0800

    TapCode 填坑完毕

--- MainWindow.py
+++ MainWindow.py
@@ -1543,6 +1543,8 @@ class Ui_MainWindow(object):
         self.HTMLDecodeButton.clicked.connect(self.HTMLDecode)
         self.EscapeEncodeButton.clicked.connect(self.EscapeEncode)
         self.EscapeDecodeButton.clicked.connect(self.EscapeDecode)
+        self.TapEncodeButton.clicked.connect(self.TapEncode)
+        self.TapDecodeButton.clicked.connect(self.TapDecode)
         self.BaseTextInputButton.clicked.connect(self.BaseTextInputFunction)
         self.BaseCipherInputButton.clicked.connect(
             self.BaseCipherInputFunction)
@@ -1566,9 +1568,60 @@ class Ui_MainWindow(object):
         self.center()
 
     # functions
+
     def DelFileTempStack(self):
         item = self.FileTempStack.currentItem()
         self.FileTempStack.takeItem(self.FileTempStack.row(item))
+
+    def TapEncode(self):
+        table = {'A': 11, 'B': 12, 'C': 13, 'D': 14, 'E': 15,
+                 'F': 21, 'G': 22, 'H': 23, 'I': 24, 'J': 25,
+                 'L': 31, 'M': 32, 'N': 33, 'O': 34, 'P': 35,
+                 'Q': 41, 'R': 42, 'S': 43, 'T': 44, 'U': 45,
+                 'V': 51, 'W': 52, 'X': 53, 'Y': 54, 'Z': 55,
+                 'a': 11, 'b': 12, 'c': 13, 'd': 14, 'e': 15,
+                 'f': 21, 'g': 22, 'h': 23, 'i': 24, 'j': 25,
+                 'l': 31, 'm': 32, 'n': 33, 'o': 34, 'p': 35,
+                 'q': 41, 'r': 42, 's': 43, 't': 44, 'u': 45,
+                 'v': 51, 'w': 52, 'x': 53, 'y': 54, 'z': 55,
+                 'K': 13, 'k': 13}
+        text = self.TapTextBox.toPlainText()
+        output = ''
+        try:
+            for i in text:
+                output += str(table[i]) + ' '
+            self.TapCipherBox.setText(output)
+        except:
+            self.TapCipherBox.setText('编码出现错误!')
+        self.FileTempStack.addItem(self.TapCipherBox.toPlainText())
+
+    def TapDecode(self):
+        retable = [['A', 'B', '(C/K)', 'D', 'E'],
+                   ['F', 'G', 'H', 'I', 'J'],
+                   ['L', 'M', 'N', 'O', 'P'],
+                   ['Q', 'R', 'S', 'T', 'U'],
+                   ['V', 'W', 'X', 'Y', 'Z']]
+        text = self.TapCipherBox.toPlainText()
+        output = ''
+        temp1 = ''
+        temp2 = ''
+        try:
+            for i in text:
+                if len(temp1) == 1 and len(temp2) == 1:
+                    output += retable[int(temp1) - 1][int(temp2) - 1]
+                    temp1 = ''
+                    temp2 = ''
+                if '1' <= i <= '5':
+                    if len(temp1) == 1:
+                        temp2 = i
+                    else:
+                        temp1 = i
+            if len(temp1) == 1 and len(temp2) == 1:
+                output += retable[int(temp1)][int(temp2)]
+            self.TapTextBox.setText(output)
+        except:
+            self.TapTextBox.setText('解码出现错误!')
+        self.FileTempStack.addItem(self.TapTextBox.toPlainText())
 
     def UrlEncode(self):
         text = self.UrlTextBox.toPlainText()
