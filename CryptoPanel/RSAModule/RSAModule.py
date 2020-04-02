603969bfd3e3da255d5f27c1457849fb55f9ef47
V <643713081@qq.com>
2020-04-02 19:49:30 +0800

    update RSA pannel
    Signed-off-by: V <643713081@qq.com>

--- CryptoPanel/RSAModule/RSAModule.py
+++ CryptoPanel/RSAModule/RSAModule.py
@@ -8,9 +8,10 @@ class RSAPanel(ui_RSAPanel):
     def __init__(self):
         super(RSAPanel, self).__init__()
         self.epqButton.clicked.connect(self.Changeepq)
-        self.dnButton.clicked.connect(self.Changeedn)
+        self.dnButton.clicked.connect(self.Changedn)
         self.dpdqpqButton.clicked.connect(self.Changedpdqpq)
         self.edpnButton.clicked.connect(self.Changeedpn)
+        self.rabinButton.clicked.connect(self.Changerabin)
         # self.RSAEncodeButton.clicked.connect(self.RSAEncode)
         # self.RSADecodeButton.clicked.connect(self.RSADecode)
         # self.RSACipherBox.textChanged.connect(self.setFontColorCipher)
@@ -21,6 +22,17 @@ class RSAPanel(ui_RSAPanel):
 
     def setFontColorText(self):
         self.RSATextBox.setTextColor(QtGui.QColor(200, 200, 200))
+
+    def lableinit(self):
+        self.RSAcBox.setEnabled(False)
+        self.RSAeBox.setEnabled(False)
+        self.RSApBox.setEnabled(False)
+        self.RSAqBox.setEnabled(False)
+        self.RSAdpBox.setEnabled(False)
+        self.RSAdqBox.setEnabled(False)
+        self.RSAdBox.setEnabled(False)
+        self.RSAmBox.setEnabled(True)
+        self.RSAnBox.setEnabled(False)
 
     def Changeepq(self):
         try:
@@ -35,10 +47,16 @@ class RSAPanel(ui_RSAPanel):
                 self.RSAChooserBox[self.RSAMode], 55))
             animation.setDuration(200)
             animation.start()
+            self.lableinit()
+            self.RSAcBox.setEnabled(True)
+            self.RSAeBox.setEnabled(True)
+            self.RSApBox.setEnabled(True)
+            self.RSAqBox.setEnabled(True)
+
         except Exception as e:
             print(str(e))
 
-    def Changeedn(self):
+    def Changedn(self):
         animation = Qt.QPropertyAnimation(self)
         animation.setTargetObject(self.RSAChooser)
         animation.setPropertyName(b'pos')
@@ -49,6 +67,10 @@ class RSAPanel(ui_RSAPanel):
             self.RSAChooserBox[self.RSAMode], 55))
         animation.setDuration(200)
         animation.start()
+        self.lableinit()
+        self.RSAcBox.setEnabled(True)
+        self.RSAdBox.setEnabled(True)
+        self.RSAnBox.setEnabled(True)
 
     def Changedpdqpq(self):
         animation = Qt.QPropertyAnimation(self)
@@ -61,6 +83,12 @@ class RSAPanel(ui_RSAPanel):
             self.RSAChooserBox[self.RSAMode], 55))
         animation.setDuration(200)
         animation.start()
+        self.lableinit()
+        self.RSAcBox.setEnabled(True)
+        self.RSAdpBox.setEnabled(True)
+        self.RSAdqBox.setEnabled(True)
+        self.RSAqBox.setEnabled(True)
+        self.RSApBox.setEnabled(True)
 
     def Changeedpn(self):
         animation = Qt.QPropertyAnimation(self)
@@ -73,3 +101,24 @@ class RSAPanel(ui_RSAPanel):
             self.RSAChooserBox[self.RSAMode], 55))
         animation.setDuration(200)
         animation.start()
+        self.lableinit()
+        self.RSAcBox.setEnabled(True)
+        self.RSAdpBox.setEnabled(True)
+        self.RSAeBox.setEnabled(True)
+        self.RSAnBox.setEnabled(True)
+
+    def Changerabin(self):
+        animation = Qt.QPropertyAnimation(self)
+        animation.setTargetObject(self.RSAChooser)
+        animation.setPropertyName(b'pos')
+        animation.setStartValue(QtCore.QPoint(
+            self.RSAChooserBox[self.RSAMode], 55))
+        self.RSAMode = 5
+        animation.setEndValue(QtCore.QPoint(
+            self.RSAChooserBox[self.RSAMode], 55))
+        animation.setDuration(200)
+        animation.start()
+        self.lableinit()
+        self.RSAcBox.setEnabled(True)
+        self.RSApBox.setEnabled(True)
+        self.RSAqBox.setEnabled(True)
--- CryptoPanel/RSAModule/ui_RSAModule.py
+++ CryptoPanel/RSAModule/ui_RSAModule.py
@@ -12,15 +12,15 @@ class ui_RSAPanel(QtWidgets.QWidget):
         self.rsaChoosePanel = QtWidgets.QWidget()
         self.rsaChoosePanel.setObjectName('rsaChoosePanel')
         self.rsaChoosePanel.setStyleSheet('#rsaChoosePanel{background-color: transparent}')
-        self.rsaChoosePanel.setGeometry(0, 0, 899, 300)
+        self.rsaChoosePanel.setGeometry(0, 0, 1199, 300)
 
         self.rsaChoosePanelScroll = uni_Widget.ICTFEScrollArea(self)
-        self.rsaChoosePanelScroll.setGeometry(QtCore.QRect(0, 0, 900, 65))
+        self.rsaChoosePanelScroll.setGeometry(QtCore.QRect(0, 0, 1100, 65))
         self.rsaChoosePanelScroll.setWidget(self.rsaChoosePanel)
 
         # blue tiaotiao
         self.RSAMode = 1
-        self.RSAChooserBox = [0, 50, 240, 480, 710, 1500]
+        self.RSAChooserBox = [0, 50, 240, 480, 710, 910]
         self.RSAChooser = QtWidgets.QLabel(self.rsaChoosePanel)
         self.RSAChooser.setPixmap(
             QtGui.QPixmap('./Resources/chooser.png'))
@@ -50,10 +50,24 @@ class ui_RSAPanel(QtWidgets.QWidget):
 
         self.rabinButton = uni_Widget.ICTFEButton(self.rsaChoosePanel)
         self.rabinButton.setObjectName('rabinButton')
-        self.rabinButton.setGeometry(QtCore.QRect(10, 60, 190, 45))
+        self.rabinButton.setGeometry(QtCore.QRect(870, 10, 190, 45))
         self.rabinButton.setText('rabin attack')
 
         # end rsa change buttons
+
+        # 校验器
+        reg = QtCore.QRegExp('[0-9a-fA-F]*')
+        validator = QtGui.QRegExpValidator(self)
+        validator.setRegExp(reg)
+
+        # RSA Decrypt button
+        self.RSAattackButton = uni_Widget.ICTFEButton(self)
+        self.RSAattackButton.setObjectName('RSAattacktButton')
+        self.RSAattackButton.setGeometry(
+            QtCore.QRect(1180, 10, 220, 45))
+        self.RSAattackButton.setText('attack!')
+
+
 
         # RSA Encode button
 
@@ -61,44 +75,64 @@ class ui_RSAPanel(QtWidgets.QWidget):
         self.RSAcBox.setObjectName('RSAcBox')
         self.RSAcBox.setGeometry(QtCore.QRect(20, 80, 1380, 50))
         self.RSAcBox.setPlaceholderText('RSA C:')
+        self.RSAcBox.setValidator(validator)
+        self.RSAcBox.setEnabled(False)
 
         self.RSApBox = uni_Widget.ICTFELineBox(self)
         self.RSApBox.setObjectName('RSApBox')
         self.RSApBox.setGeometry(QtCore.QRect(20, 140, 1380, 50))
         self.RSApBox.setPlaceholderText('RSA p:')
+        self.RSApBox.setValidator(validator)
+        self.RSApBox.setEnabled(False)
 
         self.RSAqBox = uni_Widget.ICTFELineBox(self)
         self.RSAqBox.setObjectName('RSAqBox')
         self.RSAqBox.setGeometry(QtCore.QRect(20, 200, 1380, 50))
         self.RSAqBox.setPlaceholderText('RSA q:')
+        self.RSAqBox.setValidator(validator)
+        self.RSAqBox.setEnabled(False)
 
         self.RSAnBox = uni_Widget.ICTFELineBox(self)
         self.RSAnBox.setObjectName('RSAnBox')
         self.RSAnBox.setGeometry(QtCore.QRect(20, 260, 1380, 50))
         self.RSAnBox.setPlaceholderText('RSA n:')
+        self.RSAnBox.setValidator(validator)
+        self.RSAnBox.setEnabled(False)
 
         self.RSAeBox = uni_Widget.ICTFELineBox(self)
         self.RSAeBox.setObjectName('RSAeBox')
         self.RSAeBox.setGeometry(QtCore.QRect(20, 320, 1380, 50))
         self.RSAeBox.setPlaceholderText('RSA e:')
+        self.RSAeBox.setValidator(validator)
+        self.RSAeBox.setEnabled(False)
 
         self.RSAdBox = uni_Widget.ICTFELineBox(self)
         self.RSAdBox.setObjectName('RSAdBox')
         self.RSAdBox.setGeometry(QtCore.QRect(20, 380, 1380, 50))
         self.RSAdBox.setPlaceholderText('RSA d:')
+        self.RSAdBox.setValidator(validator)
+        self.RSAdBox.setEnabled(False)
+
 
         self.RSAdpBox = uni_Widget.ICTFELineBox(self)
         self.RSAdpBox.setObjectName('RSAdpBox')
         self.RSAdpBox.setGeometry(QtCore.QRect(20, 440, 1380, 50))
         self.RSAdpBox.setPlaceholderText('RSA dp:')
+        self.RSAdpBox.setValidator(validator)
+        self.RSAdBox.setEnabled(False)
 
         self.RSAdqBox = uni_Widget.ICTFELineBox(self)
         self.RSAdqBox.setObjectName('RSAdqBox')
         self.RSAdqBox.setGeometry(QtCore.QRect(20, 500, 1380, 50))
         self.RSAdqBox.setPlaceholderText('RSA dq:')
+        self.RSAdqBox.setValidator(validator)
+        self.RSAdqBox.setEnabled(False)
 
         self.RSAmBox = uni_Widget.ICTFELineBox(self)
         self.RSAmBox.setObjectName('RSAmBox')
         self.RSAmBox.setGeometry(QtCore.QRect(20, 560, 1380, 50))
         self.RSAmBox.setPlaceholderText('RSA m:')
+        self.RSAmBox.setValidator(validator)
+        self.RSAmBox.setEnabled(False)
+
         # end RSA panel
