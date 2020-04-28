0a20fdca4975a60c8ec1f00007563625ea2d0699
Reverier-Xu <reverier.xu@outlook.com>
2020-04-28 18:22:29 +0800

    二进制小端序拆分模块
    Signed-off-by: Reverier-Xu <reverier.xu@outlook.com>

---
+++ Modules/DataFlow/Hex2BytesModule.py
@@ -0,0 +1,19 @@
+import traceback
+
+properties = {
+    'name': 'Hex To Bytes',
+    'categories': '数据转换',
+    'input': {0: '输入'},
+    'output': {0: '输出'},
+    'properties': {
+    }
+}
+
+defaults = {}
+
+
+def main(inp, settings):
+    try:
+        return {0: bytes.fromhex(inp[0])}
+    except:
+        traceback.print_exc()
