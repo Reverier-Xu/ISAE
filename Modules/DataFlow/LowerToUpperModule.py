3cb2fb00d530576f82980b1e89bd513088ff3ed2
Reverier-Xu <reverier.xu@outlook.com>
2020-05-09 21:28:04 +0800

    add some useful modules
    Signed-off-by: Reverier-Xu <reverier.xu@outlook.com>

---
+++ Modules/DataFlow/LowerToUpperModule.py
@@ -0,0 +1,13 @@
+properties = {
+    'name': 'a -> A',
+    'categories': '字符串操作',
+    'input': {0: '输入'},
+    'output': {0: '输出'},
+    'properties': {}
+}
+
+defaults = {}
+
+
+def main(inp, settings):
+    return {0: inp[0].upper()}
