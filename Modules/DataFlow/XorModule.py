d7a6b5ff760e1e25cabaf39dc19840fcea1127fc
Reverier-Xu <reverier.xu@outlook.com>
2020-05-12 22:10:09 +0800

    按字符异或
    Signed-off-by: Reverier-Xu <reverier.xu@outlook.com>

---
+++ Modules/DataFlow/XorModule.py
@@ -0,0 +1,23 @@
+properties = {
+    'name': 'xor',
+    'categories': '字符串处理',
+    'input': {0: 'origin', 1: 'pattern'},
+    'output': {0: 'output'},
+    'properties': {}
+}
+
+defaults = {}
+
+
+def main(inp, settings):
+    origin = repr(inp[0])
+    pattern = repr(inp[1])
+    i = 0
+    j = 0
+    out = ''
+    while i < len(origin):
+        out += chr(ord(origin[i]) ^ ord(pattern[j]))
+        i += 1
+        j += 1
+        j %= len(pattern)
+    return {0: out}
