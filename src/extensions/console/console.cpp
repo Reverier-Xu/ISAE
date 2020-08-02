939203d5ad9e73fa6dc42998c5aee7a941dd59b2
Reverier-Xu <reverier.xu@outlook.com>
2020-08-02 09:05:20 +0800

    UpdateTerm

---
+++ src/extensions/console/console.cpp
@@ -0,0 +1,5 @@
+//
+// Created by reverier on 2020/8/14.
+//
+
+#include "console.h"
---
+++ src/extensions/console/console.h
@@ -0,0 +1,14 @@
+//
+// Created by reverier on 2020/8/14.
+//
+
+#ifndef ISAE_CONSOLE_H
+#define ISAE_CONSOLE_H
+
+
+class console {
+
+};
+
+
+#endif //ISAE_CONSOLE_H
